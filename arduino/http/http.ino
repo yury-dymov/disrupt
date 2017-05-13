a5b5505bf16e3b188848bb01b7eb91f00aa5e6a3
Yury Dymov <yury@dymov.me>
2017-05-13 10:44:46 -0700

    toggling lock

--- arduino/http/http.ino
+++ arduino/http/http.ino
@@ -1,15 +1,28 @@
+/*
+ *  HTTP over TLS (HTTPS) example sketch
+ *
+ *  This example demonstrates how to use
+ *  WiFiClientSecure class to access HTTPS API.
+ *  We fetch and display the status of
+ *  esp8266/Arduino project continuous integration
+ *  build.
+ *
+ *  Created by Ivan Grokhotkov, 2015.
+ *  This example is in public domain.
+ */
+
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "DisruptNYC";
 const char* password = "";
 
-const char* host = "api.github.com";
+const char* host = "thingspace.io";
 const int httpsPort = 443;
+String url = "/get/latest/dweet/for/parkbikeclub";
 
-// Use web browser to view and copy
-// SHA1 fingerprint of the certificate
-const char* fingerprint = "CF 05 98 89 CA FF 8E D8 5E 5C E0 C2 E4 F7 E6 C3 C7 50 DD 5C";
+bool locked = false;
 
 void setup() {
   Serial.begin(115200);
@@ -27,30 +40,23 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // Use WiFiClientSecure class to create TLS connection
-  WiFiClientSecure client;
   Serial.print("connecting to ");
   Serial.println(host);
+}
+
+void loop() {
+  WiFiClientSecure client;
+
   if (!client.connect(host, httpsPort)) {
     Serial.println("connection failed");
     return;
   }
-
-  if (client.verify(fingerprint, host)) {
-    Serial.println("certificate matches");
-  } else {
-    Serial.println("certificate doesn't match");
-  }
-
-  String url = "/repos/esp8266/Arduino/commits/master/status";
-  Serial.print("requesting URL: ");
-  Serial.println(url);
-
+    
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Connection: close\r\n\r\n");
 
-  Serial.println("request sent");
   while (client.connected()) {
     String line = client.readStringUntil('\n');
     if (line == "\r") {
@@ -59,17 +65,24 @@ void setup() {
     }
   }
   String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\"")) {
-    Serial.println("esp8266/Arduino CI successfull!");
-  } else {
-    Serial.println("esp8266/Arduino CI has failed");
-  }
-  Serial.println("reply was:");
-  Serial.println("==========");
-  Serial.println(line);
-  Serial.println("==========");
-  Serial.println("closing connection");
-}
+  
+  StaticJsonBuffer<512> jsonBuffer;
 
-void loop() {
+  JsonObject& root = jsonBuffer.parseObject(line);
+
+  const char* payload = root["with"][0]["content"]["locked"];
+
+  bool haveToLock = false;
+
+  if (!strcmp(payload, "true")) {
+    haveToLock = true;
+  }
+
+  if (haveToLock != locked) {
+    locked = haveToLock;
+    Serial.print("locking");
+    delay(6000);
+  }
+  
+  delay(500);  
 }
