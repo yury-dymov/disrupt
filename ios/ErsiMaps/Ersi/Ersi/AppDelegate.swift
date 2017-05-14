5932d2dc2c03630cb61566c2da419b93e36f9cca
Alexander Sivura <asivura@gmail.com>
2017-05-14 09:01:03 -0400

    icons added

--- ios/ErsiMaps/Ersi/Ersi/AppDelegate.swift
+++ ios/ErsiMaps/Ersi/Ersi/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TouchVisualizer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+
+        Visualizer.start()
         VerizonClient.registerCallback()
         
         window = UIWindow()
