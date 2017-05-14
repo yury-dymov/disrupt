04dc626fd0a6844f639b76f39e38532dae16c6f6
Alexander Sivura <asivura@gmail.com>
2017-05-14 02:16:35 -0400

    Maps added

--- ios/ErsiMaps/Ersi/Ersi/MapViewController.swift
+++ ios/ErsiMaps/Ersi/Ersi/MapViewController.swift
@@ -12,12 +12,35 @@ import ArcGIS
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: AGSMapView!
+
+    let initialLatitued =  40.7095646;
+    let initialLongitude = -73.9870830;
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.mapView.map = AGSMap(basemapType: .darkGrayCanvasVector, latitude: 40.7095646, longitude: -73.9870830, levelOfDetail: 17)
+        self.mapView.viewpointChangedHandler = { callback in
+            print("Zoom \(String(describing: self.mapView.currentViewpoint(with: .centerAndScale)?.targetScale))")
+        }
+
+//        self.mapView.locationDisplay.start { [weak self] (error: Error?) -> Void in
+//            if error == nil {
+//                print("\(error)");
+//            }
+//        }
+
+
+        self.mapView.map = AGSMap(basemapType: .darkGrayCanvasVector, latitude: initialLatitued, longitude: initialLongitude, levelOfDetail: 10)
+
+
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.mapView.setViewpoint(AGSViewpoint(center: AGSPoint(clLocationCoordinate2D: CLLocationCoordinate2D(latitude: initialLatitued, longitude: initialLongitude)), scale: 4200.0), duration: 2.0)
 
+
+    }
+    
 
 
 
