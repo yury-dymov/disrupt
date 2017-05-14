16efa7e856a1b96bf7a8fc16a0ca140e0bc1b274
Yury Dymov <yury@dymov.me>
2017-05-13 21:40:39 -0400

    working implementation

--- ser.rb
+++ ser.rb
@@ -1,19 +1,41 @@
 require 'rubygems'  
 require 'serialport' 
 
-
-#params for serial port  
-port_str = "/dev/cu.usbmodem41"  #may be different for you  
-baud_rate = 9600  
-data_bits = 8  
-stop_bits = 1  
-parity = SerialPort::NONE  
-
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)  
-
-0.upto(3) do |i|
-  sp.puts(2)
-  sleep(1)
+def getmodems
+  io = IO.popen '/bin/ls /dev', 'r'
+  $stdout = io  
+  $stdout = IO.new 0
+  r = io.read.split("\n").select {|l| l =~ /cu.usbmodem/}
+  io.close
+  r
 end
 
+def _write(mode)
+  getmodems.each do |m|
+    begin
+      port_str = "/dev/#{m}"
+      baud_rate = 9600  
+      data_bits = 8  
+      stop_bits = 1  
+      parity = SerialPort::NONE  
 
+      sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)  
+
+      0.upto(3) do |i|
+        sp.puts(mode)
+        sleep(1)
+      end      
+    rescue
+    end
+  end
+end
+
+def open
+  _write(1)
+end
+
+def close
+  _write(2)
+end
+
+close
